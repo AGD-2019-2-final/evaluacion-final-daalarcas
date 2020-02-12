a545f0b55dd274eb3714f90ed1c9e37b3fa5af79
Daniel Alarca Santamaria <daalarcas@unal.edu.co>
2020-02-11 20:09:59 -0500

    Update reducer.py

--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -1,4 +1,29 @@
+#!/usr/bin/env python3
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import string
+
+if __name__ == '__main__':
+    letra = None
+    lista = []
+        
+for line in sys.stdin:
+        line = line.strip()
+        (key,val) = line.split('\t')
+        val = float(val)
+        
+        if letra == None:
+            letra = key
+        
+        if letra == key: 
+            lista.append((val))
+        else:
+            suma = sum(lista)
+            promedio = suma / float(len(lista))
+            print(letra +  '\t' + str(suma)+  '\t' + str(promedio))
+            letra = key
+            lista= []
+            lista.append((val))
+
+suma = sum(lista)
+promedio = suma / float(len(lista))
+print(letra + '\t' + str(suma)+  '\t' + str(promedio))
