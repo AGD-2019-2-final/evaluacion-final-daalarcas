a3f5bb51b6bdab2edbe5a57eabf039a780e6e753
Daniel Alarca Santamaria <daalarcas@unal.edu.co>
2020-02-11 20:09:16 -0500

    Update mapper.py

--- 01-hadoop-50/q04-10/mapper.py
+++ 01-hadoop-50/q04-10/mapper.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
-
+if __name__ == "__main__":
+    for line in sys.stdin:
+        col1 = ""
+        line = line.strip()
+        splits = line.split("   ")
+        col1 = splits[0]
+        print(col1 + '\t1')
