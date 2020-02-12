b26d3cb6b412405d47f956b256235b5a5b566c29
Daniel Alarca Santamaria <daalarcas@unal.edu.co>
2020-02-11 20:08:51 -0500

    Update reducer.py

--- 01-hadoop-50/q02-10/reducer.py
+++ 01-hadoop-50/q02-10/reducer.py
@@ -1,4 +1,23 @@
+#!/usr/bin/env python3
 import sys
-#
-#  >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import string
+
+if __name__ == '__main__':
+    (last, maxi) = (None, 0)
+    
+    for line in sys.stdin:
+        line = line.strip()
+        (key, val) = line.split('\t')
+        val = int(val)
+        maxi = int(maxi)
+    
+        if last and last != key:
+            print(last + '\t' + str(maxi))
+            (last, maxi) = (key, val)
+        else:
+            (last, maxi) = (key, max(maxi,val))      
+        
+    if last:
+        print(last + '\t' + str(maxi))
+
+    
