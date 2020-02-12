dfdce70ee4aeda77210e733cabbbd5849336f75e
Daniel Alarca Santamaria <daalarcas@unal.edu.co>
2020-02-11 20:10:08 -0500

    Update reducer.py

--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python3
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import string
+
+if __name__ == '__main__':
+    lista = []
+    
+    for line in sys.stdin:
+        line = line.strip()
+        (key, date, val) = line.split('\t')
+        val = int(val)
+           
+        lista.append((key, date, val))
+     
+    lista = sorted(lista, key=lambda tup:(tup[2]))
+
+
+    for i in lista:
+        if i[2] < 8:
+            print(i[0] + ' ' + str(i[1]) + ' ' + str(i[2]))
