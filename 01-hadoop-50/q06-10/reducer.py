53a975096bbff60ff5beceb786f3fb51cdf0eff6
Daniel Alarca Santamaria <daalarcas@unal.edu.co>
2020-02-11 20:09:46 -0500

    Update reducer.py

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -1,4 +1,27 @@
+#!/usr/bin/env python3
 import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import string
+
+if __name__ == '__main__':
+    (last, maxi , mini) = (None, 0 , 0)
+        
+    for line in sys.stdin:
+        line = line.strip()
+        (key, val) = line.split('\t')
+        val = float(val)
+        maxi = float(maxi)
+        mini = float(mini)
+        
+        if last == None:
+            mini = val
+    
+        if last and last != key:
+            print(last + '\t' + str(maxi) + '\t' + str(mini))
+            (last, maxi , mini) = (key, val , val)
+        else:
+            (last, maxi , mini) = (key, max(maxi,val) , min(mini,val))    
+        
+    if last:
+        print(last + '\t' + str(maxi) + '\t' + str(mini))
+
+    
