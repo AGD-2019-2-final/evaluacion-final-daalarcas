79432651deb2e78a874d60d050494306b0a8130f
Daniel Alarca Santamaria <daalarcas@unal.edu.co>
2020-02-11 20:09:10 -0500

    Update reducer.py

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python3
 import sys
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import string
+
+if __name__ == '__main__':
+    lista = []
+    
+    for line in sys.stdin:
+        line = line.strip()
+        (key, val) = line.split(',')
+        val = int(val)
+           
+        lista.append((key, val))
+    
+    lista = sorted(lista, key=lambda tup:tup[1])
+    
+    for i in lista:
+        print(i[0] + ',' + str(i[1]))
