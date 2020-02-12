1108362247f7760d17d39e91351ddcd7ae311588
Daniel Alarca Santamaria <daalarcas@unal.edu.co>
2020-02-11 20:09:06 -0500

    Update mapper.py

--- 01-hadoop-50/q03-10/mapper.py
+++ 01-hadoop-50/q03-10/mapper.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python3
 import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+if __name__ == "__main__":
+	for line in sys.stdin:
+		# Setting some defaults
+		letra = ""
+		num = 0
+		#amount = int(amount)
+        
+		line = line.strip()
+		splits = line.split(",")
+		#print(str(len(splits)) + ' :: ' + line)
+		#if len(splits) != 4: # Transactions have more columns than users
+		letra = splits[0]
+		num = splits[1]
+                
+			#product_id = splits[1]
+		#else:
+			#user_id = splits[0]
+			#location = splits[3] 
+		a = (letra + ',' + num)
+		print(a)  
